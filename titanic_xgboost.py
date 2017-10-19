e835b3cbcb00919c5314c403c77143cbd074f7a9
hsmtknj <emoke.any@gmail.com>
2017-10-19 22:39:33 +0900

    modify titanic_sgboost.py

--- titanic_xgboost.py
+++ titanic_xgboost.py
@@ -1,67 +1,93 @@
-u"""タイタニック号乗客の生存予想モデル."""
-
 # coding: utf-8
+"""タイタニック号乗客の生存予想モデル."""
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-from sklearn import svm
-from xgboost import XGBRegressor
+import xgboost as xgb
 
-# read train data (csv file)
-train_df = pd.read_csv("data/train.csv").replace("male", 0).replace("female", 1)
 
-# 欠損値の処理
-# 中央値を挿入
-train_df["Age"].fillna(train_df.Age.median(), inplace=True)
+def convert_data(df_input):
+    '''
+    convert features of data to create new features.
+        Input  : (pandas.core.frame.DataFrame) <- 用はpandasのデータ
+        Output : (pandas.core.frame.DataFrame) <- データ整形後の新しい特徴が追加されたデータ
+    '''
 
-# データ整形
-# 兄弟の数(SibSp)と両親・子供の数(Parch)と自分の数で新しい属性を作成
-train_df["FamilySize"] = train_df["SibSp"] + train_df["Parch"] + 1
-# 不要なカラム（列）を削除
-train_df2 = train_df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+    # 欠損値の処理(中央値を挿入)
+    df_input["Age"].fillna(df_input.Age.median(), inplace=True)
 
-# 学習データ準備
-train_data = train_df2.values
-train_x = train_data[:, 2:]  # Pclass以降の変数
-train_y = train_data[:, 1]  # 正解データ
+    # 兄弟の数(SibSp)と両親・子供の数(Parch)と自分の数で新しい特徴を作成
+    df_input["FamilySize"] = df_input["SibSp"] + df_input["Parch"] + 1
 
-# svmによる学習モデルの生成
-svm = svm.LinearSVC(C=1.0)
-svm.fit(train_x, train_y)
+    # 不要なカラム（列）を削除
+    df_output = df_input.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
 
-# テスト
-test_df = pd.read_csv("data/test.csv").replace("male", 0).replace("female", 1)
-# 欠損値の補完
-test_df["Age"].fillna(train_df.Age.median(), inplace=True)
-test_df["FamilySize"] = test_df["SibSp"] + test_df["Parch"] + 1
-test_df2 = test_df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+    return(df_output)
 
-# 予測データとアウトプット
-test_data = test_df2.values
-test_x = test_data[:, 1:]
-test_y = svm.predict(test_x)
 
-# アウトプットの書き出し用データ作成
-zip_data = zip(test_data[:, 0].astype(int), test_y.astype(int))
-predict_data = list(zip_data)
+def split_data(mat_input):
+    '''
+    split data into Features data and Supervised data
+        Input  : (numpy型の行列)
+        OUtput : (Features data(numpy), Supervised data(numpy))
+    '''
 
-# アウトプットの書き出し
-with open("result/predict_result_svm.csv", "w") as f:
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow(["PassengerId", "Survived"])
-    for pid, survived in zip(test_data[:, 0].astype(int), test_y.astype(int)):
-        writer.writerow([pid, survived])
+    x_train = mat_input[:, 2:]  # features data
+    y_train = mat_input[:, 1]  # supervised data
 
-# 精度の算出
-answer_df = pd.read_csv("data/gender_submission.csv")
-answer_data = answer_df.values
-answer_vec = answer_data[:, 1]
+    return(x_train, y_train)
 
-# answer_vec（正解データ）とtest_y（予測データ）を比較
-count = 0
-for i in range(0, len(answer_vec)):
-    if answer_vec[i] == test_y[i]:
-        count += 1
-print("accuracy: " + str(count / len(answer_vec)))
+
+def main():
+    '''main function.'''
+
+    # read train and supervised data (csv file)
+    df_train = pd.read_csv("data/train.csv").replace("male", 0).replace("female", 1)
+    df_test = pd.read_csv("data/test.csv").replace("male", 0).replace("female", 1)
+
+    # data analysis -----------------------------------------------------------
+    # percentage of survived or passed away
+    # print (df_train.head())  # 先頭から5行分表示
+    # print(df_train.Survived.value_counts())
+    # print(df_train.Survived.value_counts(normalize=True))
+
+    # percentage of survived or passed away in terms of Sex
+    # print("--- male ---")
+    # print(df_train[df_train.Sex == 0].Survived.value_counts())
+    # print(df_train[df_train.Sex == 0].Survived.value_counts(normalize=True))
+    # print("--- woman ---")
+    # print(df_train[df_train.Sex == 1].Survived.value_counts())
+    # print(df_train[df_train.Sex == 1].Survived.value_counts(normalize=True))
+    # -------------------------------------------------------------------------
+
+    # 学習データ整形
+    df_train2 = convert_data(df_train)
+    # 学習データ準備
+    (x_train, y_train) = split_data(df_train2.values)
+
+    # xgboostによる学習モデルの生成
+    xgb_model = xgb.XGBClassifier()
+    xgb_model.fit(x_train, y_train)
+
+    # テストデータ整形
+    df_test2 = convert_data(df_test)
+    # テストデータ準備・予想
+    mat_test = df_test2.values
+    x_test = mat_test[:, 1:]
+    y_test = xgb_model.predict(x_test)
+
+    # アウトプットの書き出し用データ作成
+    zip_data = zip(mat_test[:, 0].astype(int), y_test.astype(int))
+    mat_predict = list(zip_data)
+
+    # アウトプットの書き出し
+    with open("result/predict_result.csv", "w") as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow(["PassengerId", "Survived"])
+        for pid, survived in zip(mat_test[:, 0].astype(int), y_test.astype(int)):
+            writer.writerow([pid, survived])
+
+if __name__ == '__main__':
+    main()
